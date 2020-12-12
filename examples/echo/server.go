3f737b0c65df6f2921c02d23da74b6f9cde67b07
gungfu2012 <gungfu2012@hotmail.com>
2020-12-12 14:40:10 +0800

    Update server.go

--- examples/echo/server.go
+++ examples/echo/server.go
@@ -14,7 +14,7 @@ import (
 
 	"github.com/gorilla/websocket"
 )
-var addr = flag.StringVar(&addr,"addr", ":8080", "http service address")
+var addr = flag.StringVar("addr", ":8080", "http service address")
 //var addr = flag.String("addr", "localhost:8080", "http service address")
 
 var upgrader = websocket.Upgrader{} // use default options
